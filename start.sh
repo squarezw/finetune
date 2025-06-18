eed41397423bedacfd91136f20b64b32533759eb
Jesse <15653378+squarezw@user.noreply.gitee.com>
2025-06-18 19:50:18 +0800

    specific version.

--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
 docker run -itd --name ml-env --gpus all -v "$(pwd)":/workspace -w /workspace pytorch/pytorch:latest bash
+docker exec ml-env apt-get update
+docker exec ml-env apt-get install -y gcc g++
