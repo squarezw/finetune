37e356ed7dfe409e95008f54513b9f8447061a12
Jesse <15653378+squarezw@user.noreply.gitee.com>
2025-06-19 00:35:14 +0800

    Fixed split str issue

--- finetune-unsloth.py
+++ finetune-unsloth.py
@@ -46,7 +46,7 @@ def extract_xml_answer(text: str) -> str:
     return answer.strip()
 
 def extract_hash_answer(text: str) -> str | None:
-    if '###' not in text:
+    if '####' not in text:
         return None
     return text.split("###")[1].strip()
 
