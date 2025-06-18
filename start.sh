db95543f087bec4310297d2b87b3539225f48a76
Jesse <15653378+squarezw@user.noreply.gitee.com>
2025-06-19 00:27:08 +0800

    Fixed dependency.

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 docker run -itd --name ml-env --gpus all -v "$(pwd)":/workspace -w /workspace pytorch/pytorch:latest bash
-docker exec ml-env apt-get update
-docker exec ml-env apt-get install -y gcc g++
+
+# 容器内
+# apt-get update
+# apt-get install -y gcc g++
+# source /opt/conda/etc/profile.d/conda.sh
+# conda create -n myenv python=3.12
+# conda activate myenv
+# pip install -r requirements.txt
+# pip install -e .
